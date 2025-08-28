59b49ccb3e52c8c5d22c809c7b9ad39f85746f77
jatin-j9 <jatts0010@email.com>
2025-08-29 01:11:30 +0530

    added eslint and storybook configurations

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,7 @@
-export const Button = () => {
-  return <button className='text-blue-500'>Button</button>;
+import type { ComponentProps } from 'react';
+
+type ButtonProps = ComponentProps<'button'>;
+
+export const Button = ({ ...props }: ButtonProps) => {
+  return <button className='text-blue-500' {...props} />;
 };
